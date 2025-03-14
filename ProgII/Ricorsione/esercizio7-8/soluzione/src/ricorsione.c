9bba9fbf44ccfb4796ae797bc4b6564e4e3ada4e
Daniele Baccega <94167431+daniele-baccega@users.noreply.github.com>
2025-03-14 09:34:39 +0100

    Update ricorsione.c

--- ProgII/Ricorsione/esercizio7-8/soluzione/src/ricorsione.c
+++ ProgII/Ricorsione/esercizio7-8/soluzione/src/ricorsione.c
@@ -8,10 +8,3 @@ int ricerca_binaria(int arr[], int sinistra, int destra, int valore) {
     if (valore < arr[metà]) return ricerca_binaria(arr, sinistra, metà - 1, valore);
     return ricerca_binaria(arr, metà + 1, destra, valore);
 }
-
-
-int fibonacci(int n) {
-    if (n == 0) return 0;
-    if (n == 1) return 1;
-    return fibonacci(n - 1) + fibonacci(n - 2);
-}
