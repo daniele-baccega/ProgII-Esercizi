6ef40f297f0fa72405e80545d853a4eb9ff67c09
Daniele Baccega <94167431+daniele-baccega@users.noreply.github.com>
2025-03-14 09:35:00 +0100

    Update main.c

--- ProgII/Ricorsione/esercizio7-8/soluzione/src/main.c
+++ ProgII/Ricorsione/esercizio7-8/soluzione/src/main.c
@@ -17,15 +17,5 @@ int main() {
         printf("Elemento non trovato.\n");
     }
 
-    //Fibonacci
-
-
-    int n;
-    printf("Inserisci un numero intero positivo: ");
-    scanf("%d", &n);
-
-    int risultato_fibonacci = fibonacci(n);
-    printf("Il %d-esimo numero della sequenza di Fibonacci è: %d\n", n, risultato_fibonacci);
-
     return 0;
 }
